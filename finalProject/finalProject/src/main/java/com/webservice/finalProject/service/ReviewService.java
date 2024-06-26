1c49c3983e08f8e8d9a268af63b73f0c963de37e
qtw97 <qiutianw@gmail.com>
2024-06-26 14:10:10 -0400

    Reapply "review"
    This reverts commit 94c9a474cbebdbc0147a95f8ef831199a254b36b.

---
+++ finalProject/finalProject/src/main/java/com/webservice/finalProject/service/ReviewService.java
@@ -0,0 +1,36 @@
+package com.webservice.finalProject.service;
+
+import com.webservice.finalProject.model.Review;
+import com.webservice.finalProject.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    @Autowired
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public List<Review> getAllReviews() {
+        return reviewRepository.getAllReviews();
+    }
+
+    public Review getReviewById(Long id) {
+        return reviewRepository.getReviewById(id);
+    }
+
+    public Review saveReview(Review review) {
+        reviewRepository.saveReview(review);
+        return review;
+    }
+
+    public void deleteReview(Long id) {
+        reviewRepository.deleteReview(id);
+    }
+}
