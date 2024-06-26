edbfe66070294fd5ca28f475091954784bdc9ac1
qtw97 <qiutianw@gmail.com>
2024-06-26 18:31:01 -0400

    Update MovieRatings.java

---
+++ finalProject/finalProject/src/main/java/com/webservice/finalProject/model/MovieRatings.java
@@ -0,0 +1,12 @@
+package com.complete.lastversion.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MovieRatings {
+    private String title;
+    private String imdbRating;
+    private String imdbVotes;
+}
