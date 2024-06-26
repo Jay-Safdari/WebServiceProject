751afebbe987886b181433cc45757dcea3150502
qtw97 <qiutianw@gmail.com>
2024-06-26 19:49:51 -0400

    Update MovieRatingsService.java

--- finalProject/finalProject/src/main/java/com/webservice/finalProject/service/MovieRatingsService.java
+++ finalProject/finalProject/src/main/java/com/webservice/finalProject/service/MovieRatingsService.java
@@ -3,22 +3,36 @@ package com.complete.lastversion.service;
 import com.complete.lastversion.adaptor.OMDbAdaptor;
 import com.complete.lastversion.dto.MovieDTO;
 import com.complete.lastversion.model.MovieRatings;
+import com.complete.lastversion.repository.MovieRatingsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieRatingsService {
 
     private final OMDbAdaptor omdbAdaptor;
+    private final MovieRatingsRepository ratingsRepository;
 
     @Autowired
-    public MovieRatingsService(OMDbAdaptor omdbAdaptor) {
+    public MovieRatingsService(OMDbAdaptor omdbAdaptor, MovieRatingsRepository ratingsRepository) {
         this.omdbAdaptor = omdbAdaptor;
+        this.ratingsRepository = ratingsRepository;
+    }
+
+    public MovieRatings saveRatings(String title) {
+        MovieDTO movieDTO = omdbAdaptor.getMovieInfoByTitle(title);
+        MovieRatings ratings = extractRatings(movieDTO);
+        ratingsRepository.save(ratings);
+        return ratings;
     }
 
     public MovieRatings getRatings(String title) {
-        MovieDTO movieDTO = omdbAdaptor.getMovieInfoByTitle(title);
-        return extractRatings(movieDTO);
+        try {
+            return ratingsRepository.findByTitle(title);
+        } catch (EmptyResultDataAccessException e) {
+            return saveRatings(title);
+        }
     }
 
     private MovieRatings extractRatings(MovieDTO movieDTO) {
