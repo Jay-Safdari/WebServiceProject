2bb6359bbdd8ae16e48d4754609e80e9cdb26053
alexielag20 <161341410+alexielag20@users.noreply.github.com>
2024-06-26 21:14:06 -0400

    ProductionController

---
+++ finalProject/finalProject/src/main/java/com/webservice/finalProject/controller/MovieProductionController.java
@@ -0,0 +1,28 @@
+package com.webservice.finalProject.controller;
+
+import com.webservice.finalProject.model.MovieProduction;
+import com.webservice.finalProject.service.MovieProductionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("movie")
+public class MovieProductionController {
+
+    private final MovieProductionService productionService;
+
+    @Autowired
+    public MovieProductionController(MovieProductionService productionService) {
+        this.productionService = productionService;
+    }
+
+    @PostMapping("/{title}/production")
+    public MovieProduction saveProduction(@PathVariable String title) {
+        return productionService.saveProduction(title);
+    }
+
+    @GetMapping("/{title}/production")
+    public MovieProduction getProduction(@PathVariable String title) {
+        return productionService.getProduction(title);
+    }
+}
