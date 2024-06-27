c5473b7db3d54d9dd9ecb1fd53fae36a322403b9
alexielag20 <161341410+alexielag20@users.noreply.github.com>
2024-06-26 21:13:54 -0400

    ProductionRepository and RowMapper

---
+++ finalProject/finalProject/src/main/java/com/webservice/finalProject/repository/MovieProductionRepository.java
@@ -0,0 +1,28 @@
+package com.webservice.finalProject.repository;
+
+import com.webservice.finalProject.model.MovieProduction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MovieProductionRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public MovieProductionRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(MovieProduction movieProduction) {
+        String sql = "INSERT INTO movie_production (title, director, production, language, country) VALUES (?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, movieProduction.getTitle(), movieProduction.getDirector(), movieProduction.getProduction(), movieProduction.getLanguage(), movieProduction.getCountry());
+    }
+
+    public MovieProduction findByTitle(String title) {
+        String sql = "SELECT * FROM movie_production WHERE title = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{title}, new MovieProductionRowMapper());
+    }
+}
+
